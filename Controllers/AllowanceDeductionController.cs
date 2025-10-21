119def33b70c00198631e683c7c0213d0b5c42f2
Somu-010 <22103441@iubat.edu>
2025-10-21 13:11:45 +0600

    controllers

---
+++ Controllers/AllowanceDeductionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayrollProject.Data;
+using PayrollProject.DataModels;
+using PayrollProject.ViewModel;
+
+namespace PayrollProject.Controllers
+{
+    public class AllowanceDeductionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AllowanceDeductionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AllowanceDeductionVM viewModel)
+        {
+            var dataModel = new AllowanceDeduction();
+            dataModel.ADType = viewModel.ADType;
+            dataModel.ADName = viewModel.ADName;
+            dataModel.CalculationType = viewModel.CalculationType;
+            dataModel.Percentage = viewModel.Percentage;
+            dataModel.FixedAmount = viewModel.FixedAmount;
+            dataModel.EffectiveFrom = viewModel.EffectiveFrom;
+            _context.AllowanceDeductions.Add(dataModel);
+            _context.SaveChanges();
+            return View(viewModel);
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayrollProject.Data;
+using PayrollProject.DataModels;
+using PayrollProject.ViewModel;
+
+namespace PayrollProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index (EmployeeVM viewModel)
+        {
+            var dataModel = new Employee();
+            dataModel.FullName = viewModel.FullName;
+            dataModel.Gender = viewModel.Gender;
+            dataModel.DateOfBirth = viewModel.DateOfBirth;
+            dataModel.Designation = viewModel.Designation;
+            dataModel.Department = viewModel.Department;
+            dataModel.JoiningDate = viewModel.JoiningDate;
+            dataModel.BasicSalary = viewModel.BasicSalary;
+            dataModel.EmploymentType = viewModel.EmploymentType;
+            dataModel.BankAccountNumber = viewModel.BankAccountNumber;
+            dataModel.ShiftId = viewModel.ShiftId;
+            dataModel.Status = viewModel.Status;
+            _context.Employees.Add(dataModel);
+            _context.SaveChanges();
+            return View(viewModel);
+        }
+    }
+}
